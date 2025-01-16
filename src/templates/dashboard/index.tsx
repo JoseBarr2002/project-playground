f33e5cad83c233514975fb5da687c98845b40258
Jose Barrera <jfbarrera2002@gmail.com>
2025-01-16 13:51:30 -0600

    Learning to read

--- src/templates/dashboard/index.tsx
+++ src/templates/dashboard/index.tsx
@@ -1,37 +1,23 @@
 import MetricCard from "@components/metricCard";
 import RecentOrdersCardTable from "@components/recentOrdersCardTable";
 import SalesCardChart from "@components/salesCardChart";
+import mockMetricCardData from "./mockMetricCardData";
 
 const DashboardTemplate = () => {
   return (
     <div className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
       <div className="container mx-auto px-6 py-8">
         <h1 className="text-3xl font-semibold mb-6">Dashboard</h1>
-        <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-          <MetricCard
-            title="Total Revenue"
-            icon="💰"
-            text="$45,231.89"
-            subText="+20.1% from last month"
-          />
-          <MetricCard
-            title="Orders"
-            icon="📦"
-            text="+2350"
-            subText="+180.1% from last month"
-          />
-          <MetricCard
-            title="Customers"
-            icon="👥"
-            text="+12,234"
-            subText="+19% from last month"
-          />
-          <MetricCard
-            title="Active Tables"
-            icon="🍽️"
-            text="+573"
-            subText="+201 since last hour"
-          />
+        <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-5">
+          {mockMetricCardData.map((card, index) => (
+            <MetricCard
+              key={index}
+              title={card.title}
+              icon={card.icon}
+              text={card.text}
+              subText={card.subText}
+            />
+          ))}
         </div>
         <RecentOrdersCardTable />
         <SalesCardChart />
---
+++ src/templates/dashboard/mockMetricCardData.tsx
@@ -0,0 +1,34 @@
+const mockMetricCardData = [
+    {
+        title: "Total Revenue",
+        icon: "💰",
+        text: "$45,231.89",
+        subText: "+20.1% from last month"
+      },
+      {
+        title: "Orders",
+        icon: "📦",
+        text: "+2350",
+        subText: "+180.1% from last month"
+      },
+      {
+        title: "Customers",
+        icon: "👥",
+        text: "+12,234",
+        subText: "+19% from last month"
+      },
+      {
+        title: "Active Tables",
+        icon: "🍽️",
+        text: "+573",
+        subText: "+201 since last hour"
+      },
+      {
+    title: "Metric Card",
+    icon: "🐐",
+    text: "+1",
+    subText: "+1 since last hour",
+  }
+];
+
+export default mockMetricCardData;
